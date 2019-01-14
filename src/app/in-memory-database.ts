16c67cbe870a3b0a3e9be4a14a070b526c6021ad
Francisco do vale <francisco.vale@iviatecnologia.com.br>
2019-01-14 13:46:07 -0300

    Adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,10 +1,13 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from './pages/categories/shared/category.model';
+
+
 export class InMemoryDatabase implements InMemoryDbService {
 
   createDb() {
 
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de contas da casa' },
       { id: 2, name: 'Saúde', description: 'Plano de saúde, plano odontológico e remédios' },
       { id: 3, name: 'Lazer', description: 'Cinema, praia, parques, etc.' },
