9f3157ab08d949a34eeacf140a640459d99d4a03
Francisco do vale <francisco.vale@iviatecnologia.com.br>
2019-01-22 17:34:45 -0300

    Improving category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+
+import { InMemoryDatabase } from "./in-memory-database";
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-import { InMemoryDatabase } from "./in-memory-database";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { InMemoryDatabase } from "./in-memory-database";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
